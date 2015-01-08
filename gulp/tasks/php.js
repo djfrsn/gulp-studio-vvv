1dd41d2b6fa28e6f02e11f5823bd38f93af1d388
Dennis Jefferson <djfrsn@users.noreply.github.com>
2015-01-07 21:41:34 -0800

    php: fix issue where task overwrites files

--- gulp/tasks/php.js
+++ gulp/tasks/php.js
@@ -4,6 +4,5 @@ var gulp = require('gulp');
 gulp.task('php', function () {
 
     return gulp.src(sourced.php)
-    	.pipe(gulp.dest(sourced.app))
     	.pipe(browserSync.reload({stream:true, once: true}));
 });  
